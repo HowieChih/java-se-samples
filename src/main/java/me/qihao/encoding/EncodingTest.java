33903ed514ba0720796534520a7306c8abfd57c9
howiechih <howiechih@qq.com>
2017-06-06 17:44:45 +0800

    encoding test

--- src/main/java/me/qihao/encoding/EncodingTest.java
+++ src/main/java/me/qihao/encoding/EncodingTest.java
@@ -2,7 +2,6 @@ package me.qihao.encoding;
 
 import java.io.PrintStream;
 import java.nio.charset.Charset;
-import java.util.Properties;
 
 public class EncodingTest {
 
@@ -10,23 +9,29 @@ public class EncodingTest {
         String str = "文";
         char[] charArrays = str.toCharArray();
         System.out.println((int) charArrays[0]);
-        System.out.println((byte) charArrays[0]);
 
-        byte[] defaultEncodingBytes = str.getBytes(); // default: file.encoding UTF-8
+        byte[] defaultEncodingBytes = str.getBytes(); // with default charset - file.encoding
         byte[] specifiedEncodingBytes = str.getBytes(Charset.forName("UTF-16"));
+        printBytesToHex(defaultEncodingBytes, System.out);
+        printBytesToHex(specifiedEncodingBytes, System.out);
 
-        printBytes(defaultEncodingBytes, System.out);
-        System.out.println();
-        printBytes(specifiedEncodingBytes, System.out);
-        /*Properties properties = System.getProperties();
-        properties.list(System.out);*/
+        String decodedStr = new String(defaultEncodingBytes, Charset.forName("UTF-8"));
+        System.out.println((int) decodedStr.toCharArray()[0]);
+
+        // 字符 U+1D56B，由两个代码单元表示
+        String beyondCharLimitStr = "\uD835\uDD6B";
+        System.out.println(beyondCharLimitStr);
+        // code unit length 代码单元长度（该字符由两个代码单元编码）
+        System.out.println(beyondCharLimitStr.length());
+        // code point count 代码点数量（一个字符一个代码点）
+        System.out.println(beyondCharLimitStr.codePointCount(0, beyondCharLimitStr.length()));
     }
 
-    static void printBytes(byte[] bytes, PrintStream printStream) {
+    static void printBytesToHex(byte[] bytes, PrintStream printStream) {
         StringBuilder sb = new StringBuilder();
         for (byte b : bytes) {
             sb.append(String.format("%02X ", b));
         }
-        printStream.print(sb.toString());
+        printStream.println(sb.toString());
     }
 }
