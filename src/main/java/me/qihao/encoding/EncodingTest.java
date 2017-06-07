e349676b075308b22179d3663bce81312934c618
howiechih <howiechih@qq.com>
2017-06-07 17:33:02 +0800

    encoding test

--- src/main/java/me/qihao/encoding/EncodingTest.java
+++ src/main/java/me/qihao/encoding/EncodingTest.java
@@ -8,7 +8,8 @@ public class EncodingTest {
     public static void main(String[] args) {
         String str = "文";
         char[] charArrays = str.toCharArray();
-        System.out.println((int) charArrays[0]);
+        int x = charArrays[0];
+        System.out.println(x);
 
         byte[] defaultEncodingBytes = str.getBytes(); // with default charset - file.encoding
         byte[] specifiedEncodingBytes = str.getBytes(Charset.forName("UTF-16"));
@@ -19,13 +20,19 @@ public class EncodingTest {
         System.out.println((int) decodedStr.toCharArray()[0]);
 
         // 字符 U+1D56B，由两个代码单元表示
-        String beyondCharLimitStr = "\uD835\uDD6B";
+        String beyondCharLimitStr = "\uD835\uDD6B\uD835\uDD6C";
         System.out.println(beyondCharLimitStr);
         // code unit length 代码单元长度（该字符由两个代码单元编码）
         System.out.println(beyondCharLimitStr.length());
         // code point count 代码点数量（一个字符一个代码点）
         System.out.println(beyondCharLimitStr.codePointCount(0, beyondCharLimitStr.length()));
-
+        // 获得位置n的代码单元
+        System.out.println(Integer.toHexString(beyondCharLimitStr.charAt(1)));
+        // 获得第i个代码点
+        System.out.println(Integer.toHexString(beyondCharLimitStr.codePointAt(0)));
+        // 获取第i个代码点的下一个代码点
+        int nextCodePointIndex = beyondCharLimitStr.offsetByCodePoints(0, 1); // 从0开始，偏移量为一个代码点长度
+        System.out.println(Integer.toHexString(beyondCharLimitStr.codePointAt(nextCodePointIndex)));
         // https://docs.oracle.com/javase/tutorial/i18n/text/examples/StringConverter.java
     }
 
