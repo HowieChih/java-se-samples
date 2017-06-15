471ac4d7d5d8488bcee65c6b880248263c2666a4
chih <howiechih@qq.com>
2017-06-16 00:44:17 +0800

    generic init.

---
+++ src/main/java/me/qihao/generic/genericclass/GenericClassTest.java
@@ -0,0 +1,43 @@
+package me.qihao.generic.genericclass;
+
+/**
+ * @author qihao
+ * @version 1.0 2017-06-15
+ * @since 1.0
+ */
+public class GenericClassTest {
+    public static void main(String[] args) {
+        String[] words = {"Mary", "had", "a", "little", "lamb"};
+        Pair<String> mm = ArrayAlg.minmax(words);
+        System.out.printf("min: %s max: %s \n", mm.getFirst(), mm.getSecond());
+        System.out.println(ArrayAlg.getT("generic method"));
+    }
+}
+
+class ArrayAlg {
+
+    /**
+     * Gets the minimum and maximum of an array of strings
+     *
+     * @param array an array of strings
+     * @return a pari with the min and max value, or null if array is null or empty
+     */
+    public static Pair<String> minmax(String[] array) {
+        if (array == null || array.length == 0) {
+            return null;
+        }
+        String min = array[0], max = array[0];
+        for (String str : array) {
+            if (min.compareTo(str) > 0) {
+                min = str;
+            } else if (max.compareTo(str) < 0) {
+                max = str;
+            }
+        }
+        return new Pair<>(min, max);
+    }
+
+    public static <T> T getT(T t) {
+        return t;
+    }
+}
---
+++ src/main/java/me/qihao/generic/genericclass/Pair.java
@@ -0,0 +1,35 @@
+package me.qihao.generic.genericclass;
+
+/**
+ * 定义简单泛型类
+ *
+ * @author qihao
+ * @version 1.0 2017-06-15
+ * @since 1.0
+ */
+public class Pair<T> {
+
+    private T first;
+    private T second;
+
+    public Pair(T first, T second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public void setFirst(T first) {
+        this.first = first;
+    }
+
+    public T getSecond() {
+        return second;
+    }
+
+    public void setSecond(T second) {
+        this.second = second;
+    }
+}
