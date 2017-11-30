0b77b7167804b43e2788dbed145a774a2785f9bf
howiechih <howiechih@qq.com>
2017-11-30 17:57:54 +0800

    PipedStream.

---
+++ src/main/java/me/qihao/io/piped/PipedStream.java
@@ -0,0 +1,44 @@
+package me.qihao.io.piped;
+
+import java.io.IOException;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+
+public class PipedStream {
+
+    public static void main(String[] args) throws IOException {
+        final PipedOutputStream output = new PipedOutputStream();
+        final PipedInputStream input = new PipedInputStream(output);
+
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    output.write("Hello World".getBytes());
+                    output.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    int data = input.read();
+                    while (data != -1) {
+                        System.out.print((char) data);
+                        data = input.read();
+                    }
+                    input.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+    }
+}
