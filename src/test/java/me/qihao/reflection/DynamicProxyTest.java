6c25e0057c59d92cb34b46008f73ba3319db1cd7
qihao <qihao@houbank.com>
2019-07-29 15:39:13 +0800

    dynamic proxy

---
+++ src/test/java/me/qihao/reflection/DynamicProxyTest.java
@@ -0,0 +1,17 @@
+package me.qihao.reflection;
+
+import me.qihao.reflection.dynamicproxy.IUserDao;
+import me.qihao.reflection.dynamicproxy.ProxyFactory;
+import me.qihao.reflection.dynamicproxy.UserDao;
+import org.junit.Test;
+
+public class DynamicProxyTest {
+
+    @Test
+    public void testProxy() {
+        IUserDao target = new UserDao();
+        IUserDao proxy = (IUserDao) new ProxyFactory(target).getProxyInstance();
+        System.out.println(proxy.getClass());
+        proxy.save();
+    }
+}
